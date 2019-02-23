c6832f13ea8533fec530d15261e44dcd97327a37
dezhizhang <1541609448@qq.com>
2019-02-23 20:40:11 +0800

    配置文件

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+module.exports = {
+    mode:'development',
+    entry: { 
+        'index':['./src/page/index/index.js'],
+        'login':['./src/page/login/login.js']
+    },
+    output: {
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].js'
+    }
+}
