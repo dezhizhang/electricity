520ec20308e314032cf5d9d2fb0c591bee0c2057
dezhizhang <1541609448@qq.com>
2022-02-19 22:45:29 +0800

    feat:添加配置文件

---
+++ driver/mysql.go
@@ -0,0 +1,23 @@
+package driver
+
+import (
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+)
+
+var DB *gorm.DB
+
+func InitDB() (err error) {
+	dsn := "root:sdf@df%%$65#fdsbXT@tcp(127.0.0.1:3306)/cigua?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+
+		return err
+	}
+	DB = db
+	return nil
+}
+
+
