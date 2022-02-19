5d79858bbc1cc24b69e5c6433feac9f61209b91d
dezhizhang <1541609448@qq.com>
2022-02-19 23:28:47 +0800

    feat:生成grpc

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.17
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 	gorm.io/driver/mysql v1.3.0 // indirect
 	gorm.io/gorm v1.23.0 // indirect
 )
--- service/user.go
+++ service/user.go
@@ -6,20 +6,20 @@ import (
 	"electricity/model"
 	"electricity/proto"
 	"electricity/utils"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UserServer struct {
-
 }
-
 
 func ModelToResponse(user model.User) proto.UserInfoResponse {
 	userInfoResponse := proto.UserInfoResponse{
-		Id: user.ID,
+		Id:       user.ID,
 		Password: user.Password,
 		NickName: user.NickName,
-		Gender: user.Gender,
-		Role: int32(user.Role),
+		Gender:   user.Gender,
+		Role:     int32(user.Role),
 	}
 
 	if user.Birthday != nil {
@@ -30,23 +30,42 @@ func ModelToResponse(user model.User) proto.UserInfoResponse {
 
 // 获取用户列表
 
-func (u *UserServer) GetUserList(ctx context.Context,req *proto.PageInfo) (*proto.UserListResponse,error)  {
+func (u *UserServer) GetUserList(ctx context.Context, req *proto.PageInfo) (*proto.UserListResponse, error) {
 	// 获取用户列表
 	var users []model.User
 	result := driver.DB.Find(&users)
-	if result.Error != nil  {
-		return nil,result.Error
+	if result.Error != nil {
+		return nil, result.Error
 	}
 
 	rsp := &proto.UserListResponse{}
 	rsp.Total = int32(result.RowsAffected)
-	driver.DB.Scopes(utils.Paginate(int(req.Page),int(req.PageSize))).Find(&users)
+	driver.DB.Scopes(utils.Paginate(int(req.Page), int(req.PageSize))).Find(&users)
 
 	// 添加用户
-	for _,user := range users {
+	for _, user := range users {
 		userInfoRsp := ModelToResponse(user)
-		rsp.Data = append(rsp.Data,&userInfoRsp)
+		rsp.Data = append(rsp.Data, &userInfoRsp)
 	}
-	return rsp,nil
+	return rsp, nil
 }
 
+// 获取用户手机号
+
+func (u *UserServer) GetUserPhone(ctx context.Context, req *proto.PhoneRequest) (*proto.UserInfoResponse, error) {
+	// 通过手机号查询用户
+	var user model.User
+
+	result := driver.DB.Where(&model.User{Phone: req.Phone}).First(&user)
+
+	if result.Error != nil {
+		return nil,result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return nil,status.Errorf(codes.NotFound,"用户不存在")
+	}
+
+	userInfoRsp := ModelToResponse(user)
+	return &userInfoRsp,nil
+}
