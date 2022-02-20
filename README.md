e1ece2e4143ef50699885e5a44a65509bc87ffe8
dezhizhang <1541609448@qq.com>
2022-02-20 13:17:20 +0800

    feat:update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 电商系统
+
+### 创建用户
+
+```go
+for i := 0; i < 10; i++ {
+    birthDay := time.Unix(int64(1606832965), 0)
+    user := model.User{
+    NickName: fmt.Sprintf("刘德华%d", i),
+    Phone: fmt.Sprintf("1599247844%d", i),
+    Gender: fmt.Sprintf("男"),
+    Birthday:&birthDay,
+    Password: utils.Md5String(string("123456")),
+    
+    }
+    user.BaseModel.UpdateAt = birthDay
+    driver.DB.Save(&user)
+}
+```
--- tests/user.go
+++ tests/user.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"electricity/proto"
+	"electricity/utils"
 	"fmt"
 	"google.golang.org/grpc"
 )
@@ -12,31 +13,52 @@ var conn *grpc.ClientConn
 
 func Init() {
 	var err error
-	conn,err = grpc.Dial("127.0.0.1:8000",grpc.WithInsecure())
+	conn, err = grpc.Dial("127.0.0.1:8000", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
 
-	userClient =  proto.NewUserClient(conn)
+	userClient = proto.NewUserClient(conn)
 }
 
-func TestGetUserList()  {
-	rsp,err := userClient.GetUserList(context.Background(),&proto.PageInfo{
-		Page: 1,
+//测试用户列表
+
+func TestGetUserList() {
+	rsp, err := userClient.GetUserList(context.Background(), &proto.PageInfo{
+		Page:     1,
 		PageSize: 10,
 	})
 	if err != nil {
 		panic(err)
 	}
 
-	for _,user := range rsp.Data {
-		fmt.Println(user.Phone,user.NickName,user.Password,user.Birthday)
+	for _, user := range rsp.Data {
+		fmt.Println(user.Phone, user.NickName, user.Password, user.Birthday)
 	}
 }
 
-func main()  {
-	Init()
-	TestGetUserList()
-	conn.Close()
+// 测试创建用户
+
+func TestCreateUser() {
+	for i := 0; i < 10; i++ {
+		rsp,err := userClient.CreateUser(context.Background(),&proto.CreateUserInfo{
+			NickName: fmt.Sprintf("刘德华%d", i),
+			Phone:    fmt.Sprintf("1599247844%d", i),
+			Gender:   fmt.Sprintf("男"),
+			Birthday: int64(1606832965),
+			Password: utils.Md5String(string("123456")),
+		})
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(rsp.Id)
+
+	}
 }
 
+func main() {
+	Init()
+	TestCreateUser()
+	conn.Close()
+}
